11520a6f5ec366667b5601f5f6f6a651cc448153
김민섭 <>
2022-11-22 19:44:45 +0900

    액션바 업 버튼 추가

--- app/src/main/java/kr/co/machine_manager/MainActivity.kt
+++ app/src/main/java/kr/co/machine_manager/MainActivity.kt
@@ -2,10 +2,26 @@ package kr.co.machine_manager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)   //액션바 업버튼 visible
+    }
+
+    /**
+     * 액션바 업버튼 실행
+     */
+    override fun onSupportNavigateUp(): Boolean {
+
+        /**
+         * 액션바 업버튼 전처리
+         */
+        Log.d("onSupportNavigateUp", "up button click")
+
+        onBackPressed()
+        return super.onSupportNavigateUp()
     }
 }
